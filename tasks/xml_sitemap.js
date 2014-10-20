68c8055443a73d22a0bbb1bb8a1d491f2bb7755f
Daniel Furze <daniel@furzeface.com>
2014-10-20 20:22:40 +0100

    feat(Task): Adds XML sitemap validation headers

--- tasks/xml_sitemap.js
+++ tasks/xml_sitemap.js
@@ -48,7 +48,9 @@
       encoding: 'UTF-8'
     });
 
-    var urlset = feed.ele('urlset').att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+    var urlset = feed.ele('urlset').att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
+    .att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+    .att('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
 
     this.filesSrc.forEach(function(file) {
       // Create XML node for each entry
